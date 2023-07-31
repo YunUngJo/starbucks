e5679c62459ad3625e18ebd5b232d35864e95600
YunUngJo <hebabo_o@naver.com>
2023-07-31 11:41:23 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-/*
-* 검색창 제어
-*/
-// 검색창 요소(.search) 찾기.
-// html부분에서 첫번째 search를 찾아라
-const searchEl = document.querySelector('.search');
-// 클래스 search중 input요소를 찾아라
-const searchInputEl = searchEl.querySelector('input');
-// 검색창 요소를 클릭하면 실행.
-// 서치라는 클래스를 클릭하게되면 검색부분의 요소를 포커스 하게 만들어라.
-searchEl.addEventListener('click',function(){
-  // Logic..
-  searchInputEl.focus();
-});
-// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
-searchInputEl.addEventListener('focus',function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-searchInputEl.addEventListener('blur',function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
 /**
  * 페이지 스크롤에 따른 요소 제어
  * (여기부분 너무 어려움)
@@ -185,8 +160,3 @@ spyEls.forEach(function (spyEl) {
   .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
 
-/**
- * 올해가 몇 년도인지 계산
- */
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
